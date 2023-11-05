c5992af929b3a8f52a9920fb027508a00b906da6
katiiisha <mendeleevakate2002@gmail.com>
2023-11-05 18:42:21 +0300

    made edits
    сделала меню фиксированным с подсветкой активного пункта; переделала табы, чтобы при открыти страницы сразу же был первый активный таб

---
+++ script.js
@@ -0,0 +1,24 @@
+// if (location.hash === '' || location.hash === '#') {
+//     location.hash = 'content-1';
+// }
+
+const observer = new IntersectionObserver((entries) => { 
+    console.log(entries);
+    entries.forEach((entry) => { 
+        if (entry.isIntersecting) { 
+            document.querySelectorAll('.menu__link').forEach((link) => { 
+                let id = link.getAttribute('href').replace('#', '');
+                if (id === entry.target.id) {
+                    link.classList.add('menu__item_active')
+                } else { 
+                    link.classList.remove('menu__item_active')
+                }
+            })
+        }
+    })
+}, {
+    threshold: 0.2
+})
+document.querySelectorAll('.page').forEach(page => { 
+    observer.observe(page)
+})
